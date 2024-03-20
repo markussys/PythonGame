cc7afa34d073c4331e1f8378e49ff4e465085871
markusys <markusssviksnins@gmail.com>
2024-03-20 14:28:09 +0200

    Pievienojam ship bildi, ship failu

--- pythonSPCgame.py
+++ pythonSPCgame.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
@@ -9,6 +10,7 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen=pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship=Ship(self)
         
 
     def run_game(self):
@@ -17,6 +19,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitime()
             pygame.display.flip()
             self.clock.tick(60)
 if __name__=='__main__':
