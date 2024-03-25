898a0582fa1df67f5dfb7fa765ee4bd17aa3016d
markusys <markusssviksnins@gmail.com>
2024-03-25 16:38:57 +0200

    Izveidoju bullet fire

--- settings.py
+++ settings.py
@@ -3,3 +3,9 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
+        self.ship_speed = 1.5
+        #i need mour boulets
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (13, 13, 13)
